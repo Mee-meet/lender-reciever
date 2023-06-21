221988082d15b223d6f601797dfe309e71ad0040
mehta-meet115 <136365063+mehta-meet115@users.noreply.github.com>
2023-06-21 14:55:34 +0530

    frontend uploaded

--- App.js
+++ App.js
@@ -2,13 +2,29 @@ import React, { useState } from "react";
 import { View, TextInput, Button, StyleSheet } from "react-native";
 
 export default function App() {
-  const [lenderName, setLenderName] = useState("");
-  const [receiverName, setReceiverName] = useState("");
+  const [lender, setLenderName] = useState("");
+  const [receiver, setreceiver] = useState("");
   const [date, setDate] = useState("");
   const [amount, setAmount] = useState("");
 
-  const FormSubmit = () => {
-    //?????????
+  const FormSubmit = async () => {
+    console.warn({ lender, receiver, amount, date });
+    let data = {
+      lender, // The LenderName field in the skeleton struct corresponds to lender in the frontend code.
+      receiver,
+      amount: parseFloat(amount),
+      date,
+    };
+    // const url = "http://localhost:8080/transaction";
+
+    let result = await fetch("http://localhost:8080/transaction", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" }, // Updated content type
+      body: JSON.stringify(data),
+    });
+
+    result = await result.json();
+    console.warn(result);
   };
 
   return (
@@ -17,14 +33,14 @@ export default function App() {
         <TextInput
           style={styles.input}
           placeholder="Lender Name"
-          value={lenderName}
+          value={lender}
           onChangeText={(text) => setLenderName(text)}
         />
         <TextInput
           style={styles.input}
           placeholder="Receiver Name"
-          value={receiverName}
-          onChangeText={(text) => setReceiverName(text)}
+          value={receiver}
+          onChangeText={(text) => setreceiver(text)}
         />
         <TextInput
           style={styles.input}
